909104d76b1b27d7cfaccfe3ced8a928fa2f930f
NghiaNgo0409 <nghiango0409@gmail.com>
2023-02-14 14:16:20 +0700

    initial commit

---
+++ Singly Linked List/Ex04.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class SLinkedList
+{
+public:
+    class Node; // Forward declaration
+protected:
+    Node *head;
+    Node *tail;
+    int count;
+
+public:
+    SLinkedList();
+    ~SLinkedList();
+    void add(T e);
+    void add(int index, T e);
+    int size();
+
+public:
+    class Node
+    {
+    private:
+        T data;
+        Node *next;
+        friend class SLinkedList<T>;
+
+    public:
+        Node()
+        {
+            next = 0;
+        }
+        Node(Node *next)
+        {
+            this->next = next;
+        }
+        Node(T data, Node *next)
+        {
+            this->data = data;
+            this->next = next;
+        }
+    };
+};
+
+int main()
+{
+}
